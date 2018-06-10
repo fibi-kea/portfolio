032637b28515f4ac4509923b2afa743b7942f9da
Fríðbjartur Miðberg <fm@Fribjarturs-MacBook-Pro.local>
2018-06-10 23:19:09 +0200

    portfolio

---
+++ js/main.js
@@ -0,0 +1,80 @@
+/*! swapp text */
+$(window).on("load", start);
+
+function start() {
+    console.log("Start");
+
+    $("#header2").addClass("hidden");
+    $("#header").removeClass("hidden");
+    setTimeout(header_02, 4500);
+}
+
+function header_02() {
+    console.log("header_02");
+
+    $("#header2").removeClass("hidden");
+    $("#header").addClass("hidden");
+    setTimeout(start, 6000);
+}
+
+/*! Fade */
+$('.fade').click(function (e) {
+    e.preventDefault();
+    newLocation = this.href;
+    $('#wrapper').fadeOut('slow', newpage);
+});
+
+function newpage() {
+    window.location = newLocation;
+}
+
+
+$(document).ready(function () {
+    $('#wrapper').fadeIn('slow');
+
+});
+
+
+
+/*function Reload() {
+    try {
+        var headElement = document.getElementsByTagName("head")[0];
+        if (headElement && headElement.innerHTML)
+            headElement.innerHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
+    } catch (e) {}
+}
+
+if ((/iphone|ipod|ipad.*os 5/gi).test(navigator.appVersion)) {
+    window.onpageshow = function (evt) {
+        if (evt.persisted) {
+            document.body.style.display = "none";
+            location.reload();
+        }
+    };
+}*/
+
+/*! klik paa div */
+$(".figcaption").click(function () {
+    window.location = $(this).find("a").attr("href");
+    return false;
+});
+
+$(".figcaption").hover(function () {
+    $(this).css('cursor', 'pointer');
+}, function () {
+    $(this).css('cursor', 'auto');
+});
+
+
+$(".click01").click(function () {
+    window.location = $(this).find("a").attr("href");
+    return false;
+});
+
+$(".click01").hover(function () {
+    $(this).css('cursor', 'pointer');
+}, function () {
+    $(this).css('cursor', 'auto');
+});
+
+/*! scroll */
